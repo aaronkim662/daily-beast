c38dc059673939e5e1d33fcc68df9df593cb10c0
aaron <aaronkim662@gmail.com>
2022-03-31 17:02:41 -0400

    edited title component to show on page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React from 'react';
 import { GifLayout } from './gif-layout/gifLayout';
 import { SearchInput } from './search-input/searchInput';
+import { TitleComponent } from './title-component/title-component';
 
 const App = () => {
   return (
     <div className="App">
+      <TitleComponent />
       <SearchInput/>
       <GifLayout/>
     </div>
--- src/title-component/title-component.jsx
+++ src/title-component/title-component.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 // does not take in props
-const TitleComponent = () => {
-
+export const TitleComponent = () => {
+  return (
+  
+    <div>GIPHY GRABBER</div>
+  )
 }
