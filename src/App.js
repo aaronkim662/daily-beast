73d78a2862feb2f5df44933a1de54f4febbc4867
aaron <aaronkim662@gmail.com>
2022-03-31 17:01:28 -0400

    added search input to main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 import { GifLayout } from './gif-layout/gifLayout';
+import { SearchInput } from './search-input/searchInput';
 
 const App = () => {
   return (
     <div className="App">
+      <SearchInput/>
       <GifLayout/>
     </div>
   );
