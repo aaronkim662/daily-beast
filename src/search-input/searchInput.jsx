aa70b7ffd0534f7b840a5689143e8825a6406e24
aaron <aaronkim662@gmail.com>
2022-03-31 17:01:19 -0400

    edited search input

--- src/search-input/searchInput.jsx
+++ src/search-input/searchInput.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
-export const searchInput = () => {
+
+export const SearchInput = (props) => {
   return (
-    <div></div>
+    <form id = 'search-form'>
+      <input
+        id = 'search-input'
+        value = {props.value}
+        placeholder = 'Search'
+      />
+      <FontAwesomeIcon
+        id = 'search-icon-id'
+        className = 'search-icon'
+        icon = {faMagnifyingGlass}
+        flip = "horizontal"
+      />
+    </form>
   )
 }
