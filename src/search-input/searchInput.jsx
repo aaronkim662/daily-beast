195882e093c7c1314d600302dd962171f5602c4f
aaron <aaronkim662@gmail.com>
2022-04-02 16:17:35 -0400

    edited search component to set search state

--- src/search-input/searchInput.jsx
+++ src/search-input/searchInput.jsx
@@ -4,12 +4,14 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 
 export const SearchInput = (props) => {
+  
   return (
-    <form id = 'search-form'>
+    <form id = 'search-form' onSubmit = {(event) => props.getGifs(event)}>
       <input
         id = 'search-input'
-        value = {props.value}
+        value = {props.search}
         placeholder = 'Search'
+        onChange = {event => props.setSearch(event.target.value)}
       />
       <FontAwesomeIcon
         id = 'search-icon-id'
