eb75e80062d5ffd357055fe8974308812937bcc3
aaron <aaronkim662@gmail.com>
2022-04-04 00:33:53 -0400

    added previous container

---
+++ src/previous-container/previousContainer.jsx
@@ -0,0 +1,15 @@
+import './previousContainer.css';
+
+export const PreviousContainer = (props) => {
+  return (
+    <div className = 'previous-container'>
+      <div className = 'static-elements'>
+        <div className = 'previous-container-element'>Previous</div>
+        <div className = 'previous-container-element'>Searches:</div>
+      </div>
+      <div className='previous-container-value'>
+        {props.previous} 
+      </div>
+    </div>
+  )
+}
