9e3f6c46f7bd4f829b1ba966c3468a2d0c356fa5
aaron <aaronkim662@gmail.com>
2022-04-04 20:48:25 -0400

    added components into one center import

---
+++ src/import.js
@@ -0,0 +1,11 @@
+import PreviousContainer from './previous-container/previousContainer';
+import ResultsContainer from './results-container/resultsContainer';
+import SearchInput from './search-input/searchInput'
+import TitleContainer from './title-container/title-container';
+
+export {
+  PreviousContainer,
+  ResultsContainer,
+  SearchInput,
+  TitleContainer,
+}
